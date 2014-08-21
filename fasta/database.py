81a5229f58dc14421ff5329c93e2636181835331
Lucas Sinclair <lucas.sinclair@me.com>
2014-08-21 15:07:55 +0200

    Added a factory

--- fasta/database.py
+++ fasta/database.py
@@ -20,8 +20,7 @@ class DatabaseFASTA(Database, FASTA):
 
     def __init__(self, path=None):
         self.path = path
-        self._factory = None
-        #self.factory = lambda cursor, row: SeqRecord(Seq(row[2]), id=row[0], description=row[1])
+        self.factory = lambda cursor, row: SeqRecord(Seq(row[2]), id=row[0], description=row[1])
 
     def parse(self):
         self.open()
