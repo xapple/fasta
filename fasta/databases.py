df087e27aa8fae3269fd01c48a006eb3d7519571
Lucas Sinclair <lucas.sinclair@me.com>
2014-11-21 11:56:54 +0100

    Added the refseq prot db

--- fasta/databases.py
+++ fasta/databases.py
@@ -1,30 +1,47 @@
 # Futures #
 
 # Built-in modules #
+import fnmatch
 
 # Internal modules #
+from plumbing.autopaths import AutoPaths
 
 # Third party modules #
 from ftputil import FTPHost
+from tqdm import tqdm
+
+# Constants #
+directory = "/glob/lucass/"
 
 ###############################################################################
-class RefSeq(object):
-    """We need to download the raw sequences of the refseq protein database."""
+class RefSeqBacteriaProtNR(object):
+    """the RefSeq sequences for only bacteria, only protein, and only the
+    non-redundant version. We will download the raw sequences by FTP
+    and format them as a blast data base."""
 
-    ftp_url = "ftp://ftp.ncbi.nlm.nih.gov/refseq/release/complete/"
+    short_name = "refseq_bact_prot_nr"
+    ftp_url    = "ftp.ncbi.nlm.nih.gov"
+    ftp_dir    = "/refseq/release/bacteria/"
 
     all_paths = """
-    /lorem
+    /raw/
+    /blast_db/
     """
 
-    def __init__(self, base_dir=None, seq_type='prot'):
+    def __init__(self):
         # Attributes #
-        self.seq_type = seq_type
-        self.base_dir = base_dir
+        self.seq_type = 'prot'
+        self.base_dir = directory + self.short_name
         self.p = AutoPaths(self.base_dir, self.all_paths)
 
     def download(self):
         """Retrieve all files from the FTP site"""
-        self.ftp = FTPHost(self.ftp_url)
-        self.ftp_files = self.ftp.listdir()
-        files_we_want = files
+        self.ftp = FTPHost(self.ftp_url, "anonymous")
+        self.ftp.chdir(self.ftp_dir)
+        self.ftp_files = self.ftp.listdir(self.ftp.curdir)
+        self.pattern = 'bacteria.nonredundant_protein.*.protein.faa.gz'
+        self.files_we_want = [f for f in self.ftp_files if fnmatch.fnmatch(f, self.pattern)]
+        for f in tqdm(self.files_we_want): self.ftp.download(f, self.p.raw_dir + f)
+
+###############################################################################
+refseq_bact_prot_nr = RefSeqBacteriaProtNR()
