5795c50643603845ae5fc90c18bbbcbaf0c6cb1b
Lucas Sinclair <lucas.sinclair@me.com>
2014-11-21 16:54:17 +0100

    Better refseq db

--- fasta/databases.py
+++ fasta/databases.py
@@ -1,10 +1,10 @@
-# Futures #
-
 # Built-in modules #
 import fnmatch
+from collections import OrderedDict
 
 # Internal modules #
-from plumbing.autopaths import AutoPaths
+from plumbing.autopaths import AutoPaths, FilePath
+from plumbing.cache import property_cached
 
 # Third party modules #
 from ftputil import FTPHost
@@ -14,7 +14,48 @@ from tqdm import tqdm
 directory = "/glob/lucass/"
 
 ###############################################################################
-class RefSeqBacteriaProtNR(object):
+class Database(object):
+    """General database object to inherit from."""
+    all_paths = """
+    /raw/
+    /blast_db/
+    """
+
+    def __init__(self, seq_type):
+        # Attributes #
+        self.seq_type = seq_type
+        self.base_dir = directory + self.short_name
+        self.p = AutoPaths(self.base_dir, self.all_paths)
+
+    @property_cached
+    def ftp(self):
+        """If the data is obtained by FTP here is the ftputil object."""
+        ftp = FTPHost(self.ftp_url, "anonymous")
+        ftp.chdir(self.ftp_dir)
+        return ftp
+
+    @property_cached
+    def files_to_retrive(self):
+        """The files we want to download with their destinations."""
+        pattern = 'bacteria.nonredundant_protein.*.protein.faa.gz'
+        files = self.ftp.listdir(self.ftp.curdir)
+        return OrderedDict((f, FilePath(self.p.raw_dir+f)) for f in files
+                            if fnmatch.fnmatch(f, pattern))
+
+    @property_cached
+    def files_remaining(self):
+        """The files we haven't downloaded yet based on size checks."""
+        return OrderedDict((source,dest) for source,dest in self.files_to_retrive.items()
+                           if dest.size != self.ftp.path.getsize(source))
+
+    def download(self):
+        """Retrieve all files from the FTP site"""
+        for source,dest in tqdm(self.files_remaining.items()):
+            dest.remove()
+            self.ftp.download(source, dest)
+
+###############################################################################
+class RefSeqBacteriaProtNR(Database):
     """the RefSeq sequences for only bacteria, only protein, and only the
     non-redundant version. We will download the raw sequences by FTP
     and format them as a blast data base."""
@@ -23,25 +64,16 @@ class RefSeqBacteriaProtNR(object):
     ftp_url    = "ftp.ncbi.nlm.nih.gov"
     ftp_dir    = "/refseq/release/bacteria/"
 
-    all_paths = """
-    /raw/
-    /blast_db/
-    """
+###############################################################################
+class RefSeqArchaeaProtNR(Database):
+    """the RefSeq sequences for only bacteria, only protein, and only the
+    non-redundant version. We will download the raw sequences by FTP
+    and format them as a blast data base."""
 
-    def __init__(self):
-        # Attributes #
-        self.seq_type = 'prot'
-        self.base_dir = directory + self.short_name
-        self.p = AutoPaths(self.base_dir, self.all_paths)
-
-    def download(self):
-        """Retrieve all files from the FTP site"""
-        self.ftp = FTPHost(self.ftp_url, "anonymous")
-        self.ftp.chdir(self.ftp_dir)
-        self.ftp_files = self.ftp.listdir(self.ftp.curdir)
-        self.pattern = 'bacteria.nonredundant_protein.*.protein.faa.gz'
-        self.files_we_want = [f for f in self.ftp_files if fnmatch.fnmatch(f, self.pattern)]
-        for f in tqdm(self.files_we_want): self.ftp.download(f, self.p.raw_dir + f)
+    short_name = "refseq_bact_prot_nr"
+    ftp_url    = "ftp.ncbi.nlm.nih.gov"
+    ftp_dir    = "/refseq/release/archaea/"
 
 ###############################################################################
-refseq_bact_prot_nr = RefSeqBacteriaProtNR()
+refseq_bact_prot_nr = RefSeqBacteriaProtNR('prot')
+refseq_arch_prot_nr = RefSeqArchaeaProtNR('prot')
