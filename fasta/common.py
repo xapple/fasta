e96bb4ed4af6a4e7f13fa4f4e9d3517e87e3041e
Lucas Sinclair <lucas.sinclair@me.com>
2015-09-09 10:54:29 +0200

    Added a argument to dummy score

--- fasta/common.py
+++ fasta/common.py
@@ -3,8 +3,8 @@
 # Built-in modules #
 
 ################################################################################
-def add_dummy_scores(iteratable):
+def add_dummy_scores(iteratable, score=0):
     """Add zero scores to all sequences"""
     for seq in iteratable:
-        seq.letter_annotations["phred_quality"] = (0,)*len(seq)
+        seq.letter_annotations["phred_quality"] = (score,)*len(seq)
         yield seq
