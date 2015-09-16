191058f7d8609ae7cf421d67d84ae38ba7e3b959
Lucas Sinclair <lucas.sinclair@me.com>
2015-09-16 11:48:05 +0200

    Refactored the graph class a bit

--- fasta/graphs.py
+++ fasta/graphs.py
@@ -12,12 +12,14 @@ __all__ = ['LengthDist']
 class LengthDist(Graph):
     """The length distribution of the sequences"""
     short_name = 'length_dist'
+    sep        = ('x')
+    x_grid     = True
 
     def __init__(self, parent):
         self.parent = parent
         self.path = FilePath(self.parent.prefix_path + '_len_dist.pdf')
 
-    def plot(self, x_log=False, y_log=False):
+    def plot(self, **kwargs):
         # Data #
         counts = self.parent.lengths_counter
         # Plot #
@@ -29,12 +31,8 @@ class LengthDist(Graph):
         axes.set_title(title)
         axes.set_xlabel('Length of sequence in nucleotides')
         axes.set_ylabel('Number of sequences with this length')
-        axes.xaxis.grid(False)
-        # Add logarithm to axes #
-        if x_log: axes.set_xscale('symlog')
-        if y_log: axes.set_yscale('symlog')
         # Save it #
-        self.save_plot(fig, axes, sep=('x'))
+        self.save_plot(fig, axes, **kwargs)
         pyplot.close(fig)
         # For convenience #
         return self
