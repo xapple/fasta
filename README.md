39d35bb56dd5787be89048d5c2ed17ef75fce98c
Lucas Sinclair <lucas.sinclair@me.com>
2016-03-25 16:27:11 +0100

    "Version 1.0.3"

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 [![changelog](http://allmychanges.com/p/python/fasta/badge/)](http://allmychanges.com/p/python/fasta/?utm_source=badge) [![PyPI version](https://badge.fury.io/py/fasta.svg)](https://badge.fury.io/py/fasta)
 
-# `fasta` version 1.0.2
+# `fasta` version 1.0.3
 
 This python package enables you to deal with biological sequence files easily.
 
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name             = 'fasta',
-    version          = '1.0.2',
+    version          = '1.0.3',
     description      = 'The fasta python package enables you to deal with biological sequence files easily.',
     license          = 'MIT',
     url              = 'http://github.com/xapple/fasta/',
