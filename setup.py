964c4744be170ad42bed571b03b244fe31fe1c50
Lucas Sinclair <649288+xapple@users.noreply.github.com>
2020-01-20 21:45:03 +0100

    Added markdown content-type to setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ setup(
     name             = 'fasta',
     version          = '1.0.4',
     description      = 'The fasta python package enables you to deal with biological sequence files easily.',
+    long_description = open('README.md').read(),
+    long_description_content_type = 'text/markdown',
     license          = 'MIT',
     url              = 'http://github.com/xapple/fasta/',
     author           = 'Lucas Sinclair',
