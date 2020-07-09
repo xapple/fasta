8941da6d31df566e98d3ff04e5d68761f8d10bfb
Lucas Sinclair <649288+xapple@users.noreply.github.com>
2020-07-09 14:52:34 +0200

    Version 2.0.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name             = 'fasta',
-    version          = '2.0.5',
+    version          = '2.0.6',
     description      = 'The fasta python package enables you to deal with '
                        'biological sequence files easily.',
     license          = 'MIT',
