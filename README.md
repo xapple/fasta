011dc2ea5229c13000f0039517320442d15ff84a
Lucas Sinclair <649288+xapple@users.noreply.github.com>
2020-09-03 22:13:12 +0200

    Back to development version.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 [![PyPI version](https://badge.fury.io/py/fasta.svg)](https://badge.fury.io/py/fasta)
 
-# `fasta` version 2.2.0
+# `fasta` version 2.2.1
 
 This python package enables you to deal with biological sequence files easily.
 
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name             = 'fasta',
-    version          = '2.2.0',
+    version          = '2.2.1',
     description      = 'The fasta python package enables you to deal with '
                        'biological sequence files easily.',
     license          = 'MIT',
