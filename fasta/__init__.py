a443763bee27f61d81cff2b4778c60a810faa907
Lucas Sinclair <649288+xapple@users.noreply.github.com>
2020-09-04 15:15:15 +0200

    Version 2.2.2

--- fasta/__init__.py
+++ fasta/__init__.py
@@ -13,7 +13,7 @@ import os, sys
 # Constants #
 url         = 'http://xapple.github.io/fasta/'
 repo_url    = 'http://github.com/xapple/fasta/'
-__version__ = '2.2.1'
+__version__ = '2.2.2'
 
 # Get paths to module #
 self       = sys.modules[__name__]
