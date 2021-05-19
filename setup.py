970b35313ca747033619423859baa15f9af70997
Lucas Sinclair <649288+xapple@users.noreply.github.com>
2021-05-19 23:05:02 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,22 @@
-from setuptools import setup, find_packages
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
+"""
+Written by Lucas Sinclair.
+MIT Licensed.
+Contact at www.sinclair.bio
+"""
+
+# Imports #
+from setuptools import setup, find_namespace_packages
+from os import path
+
+# Load the contents of the README file #
+this_dir = path.abspath(path.dirname(__file__))
+readme_path = path.join(this_dir, 'README.md')
+with open(readme_path, encoding='utf-8') as handle: readme = handle.read()
+
+# Call setup #
 setup(
     name             = 'fasta',
     version          = '2.2.7',
@@ -13,7 +30,8 @@ setup(
     packages         = find_packages(),
     install_requires = ['plumbing>=2.8.5', 'autopaths>=1.4.4', 'biopython',
                         'numpy', 'sh', 'tqdm', 'matplotlib', 'regex'],
-    long_description = open('README.md').read(),
+    python_requires  = ">=3.6",
+    long_description = readme,
     long_description_content_type = 'text/markdown',
     include_package_data = True,
 )
